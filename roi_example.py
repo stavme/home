8a0200b81c1dc8def4dc7e6ee83953b9c9ba8a73
Stav Meitav <stavm4@gmail.com>
2024-12-31 15:00:07 +0200

    inserted file

---
+++ roi_example.py
@@ -0,0 +1,9 @@
+
+def comparison_promt(promt, threshold, bigger_prompt, smaller_prompt):
+    variable = int(input(promt))
+    if variable > threshold:
+        print(bigger_prompt)
+    else:
+        print(smaller_prompt)
+
+comparison_promt("בן כמה אתה?", 5, "ילד גדול", "אתה קטן מדי, תחזור פעם הבאה")
